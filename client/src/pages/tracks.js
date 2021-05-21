70dc8089d8ee38ace90aacca71cbc624b35473aa
Rebecca Sue <commissions@rbcsue.com>
2021-05-21 04:19:00 -0400

    Create fetchUser

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+export const fetchUser = gql`
+query ExampleQuery {
+  postsList {
+    id
+    author {
+      id
+      name
+      joinDate {
+        year
+        month
+        day
+        hour
+        minute
+        second
+      }
+      avatar
+      pastPosts
+    }
+  }
+}
+`;
+
+
+
 
 /**
  * Tracks Page is the Catstronauts home page.
--- server/src/schema.js
+++ server/src/schema.js
@@ -9,27 +9,60 @@ type Query {
 }
   
 type User {
+
+    "id: the unique id of this user, can't be null"
     id: ID!
+
+    "name: the display name, can't be null"
     name: String!
+
+    "joinDate: the registered date"
     joinDate: Date!
+
+    "avatar: the url of the profile photo"
+    avatar: String
+
+    "pastPosts: an array of the ID of the past posts"
     pastPosts:[ID!]
   }
 
 type Post {
+
+    "id: the unique id of this post, can't be null"
     id: ID!
+
+    "author: the person who posted this, can't be null"
     author: User!
+
+    "title: the title of this post, can't be null"
     title: String!
+
+    "body: the body of this post"
     body: String
+
+    "comment: an array of the ID of the comment of this post"
     comment: [ID!]
+    
+    "date: the time when this post was uploaded" 
     date: Date!
 }
 
 
 type Comment {
+
+    "id: the unique id of this comment, can't be null"
     id: ID!
+
+    "author: the person who posted this, can't be null"
     author: ID!
+
+    "postid: the id indicates which post this comment belongs to, can't be null"
     postid: ID!
+
+    "body: the body of this comment"
     body: String
+
+    "date: the time when this comment was uploaded"
     date: Date!
 }
 
