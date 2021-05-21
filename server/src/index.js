630ffa1a1309db5b5617466e4daae48b89d8996c
Rebecca Sue <commissions@rbcsue.com>
2021-05-21 14:01:36 -0400

    Update Server

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,10 @@ const {usersData} = require('../data/user.json');
 const {postsData} = require('../data/post.json');
 
 
+const gateway = new ApolloGateway({
+
+});
+
 
 const resolvers = {
     Query: {
@@ -29,7 +33,7 @@ const resolvers = {
 }
 
 const server = new ApolloServer({
-    typeDefs, 
+    typeDefs
 });
 
 server.listen().then(() => {
@@ -37,10 +41,10 @@ server.listen().then(() => {
       Please choose one of the options:
 
         - Print all of the user information
-            enter "fetch-all-users"
+            enter fetch-all-users
 
         - Print all of the post information
-            enter "fetch-all-posts"
+            enter fetch-all-posts
 
         - Print a user's information
             enter "user:" + the id of the user
